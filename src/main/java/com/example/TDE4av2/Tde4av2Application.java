42bc8ea2a90d47daefb2738985407feefa97660b
Henrique Gurjão <92170668+HenriqueGurjao@users.noreply.github.com>
2024-11-26 19:10:04 -0300

    Atualizar o Tde4av2Application.java

--- src/main/java/com/example/TDE4av2/Tde4av2Application.java
+++ src/main/java/com/example/TDE4av2/Tde4av2Application.java
@@ -3,6 +3,12 @@ package com.example.TDE4av2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@OpenAPIDefinition(
+		servers = {
+				@Server(url = "/", description = "Default Server URL")
+		}
+)
 @SpringBootApplication
 public class Tde4av2Application {
 
